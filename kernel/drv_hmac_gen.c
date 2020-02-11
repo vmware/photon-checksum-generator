99a9cea4e01e1c132bb9ec76a770f0ba9094334a
Keerthana K <keerthanak@vmware.com>
2020-02-11 05:15:51 +0000

    Update source files with SPDX identifier
    Signed-off-by: Keerthana K <keerthanak@vmware.com>

--- kernel/drv_hmac_gen.c
+++ kernel/drv_hmac_gen.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2020 VMware, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: GPL v2.0
  *
  * Licensed under the GNU Lesser General Public License version 2 (the "License");
  * you may not use this file except in compliance with the License. The terms
